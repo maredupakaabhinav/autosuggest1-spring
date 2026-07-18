a70ff2c1df964e9a41a0fa24ed452ba4e454f8bb
ABHINAV <maredupakaabhinav@gmail.com>
2026-07-18 12:10:19 +0530

    toggleuser

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,3 +1,32 @@
 // alert("Hello guysss");
 
 console.log("hello from JS!")
+
+
+var users = [
+    {
+        "name" : "John Doe",
+        "gender" : "Male",
+        "img" : "john.png"
+    },
+    {
+        "name" : "Jane Doe",
+        "gender" : "Female",
+        "img" : "jane.png"
+    }
+]
+
+var id = 0;
+
+function ToggleUser(){
+    id = (id+1)%2;
+    
+    var userImage = document.getElementById("user-image");
+    userImage.src = users[id].img;
+
+    var userName = document.getElementById("user-name");
+    userName.innerHTML = users[id].name;
+
+    var userGender = document.getElementById("user-gender");
+    userGender.innerHTML = users[id].gender;
+}
